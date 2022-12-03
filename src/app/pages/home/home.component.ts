f61cf78441dfce5fccc333b5696a082c5cc02817
Gafarias <gabriel_ff@outlook.com>
2022-12-02 23:40:49 -0300

    feat: finished application

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  locales = [
+    "Olá, meu nome é Gabriel Farias e eu sou um desenvolvedor Front-End, Tecnologias que tenho experiência:",
+    "Hello there, my name is Gabriel Farias and I am a Front-End developer, Technologies I have experience with:",
+    "Hola, mi nombre es Gabriel Farias y soy desarrollador Front-End, Tecnologías que tengo experiencia:"
+  ];
 
+  i = 0;
+
+  setIndex(n: number) {
+    return this.i = n
+  }
 }
